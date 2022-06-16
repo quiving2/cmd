2041a9513e9efd982ee19f8595fd68045b939a4d
quiving2 <96442436+quiving2@users.noreply.github.com>
2022-06-16 11:58:30 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("\n\nCheck the server for the new loadstring!\n\nInvite: .gg/VnWVvAMCBC)
+game.Players.LocalPlayer:Kick("\n\nCheck the server for the new loadstring!\n\nInvite: .gg/VnWVvAMCBC")
